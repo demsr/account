f27b890c755966ace5758542cf58fce27e4ce5d0
DennisSumser <dennis.sumser@protonmail.com>
2021-05-25 20:39:46 +0200

    add auth

--- server.js
+++ server.js
@@ -7,7 +7,36 @@ const session = require("express-session");
 const Redis = require("ioredis");
 const redis = new Redis();
 
+/* Auth stuff */
+const passport = require("passport");
+const connectEnsureLogin = require("connect-ensure-login");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/user");
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username." });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 app.use(express.static("public"));
 
 app.use(
@@ -19,7 +48,7 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
+app.get("/", connectEnsureLogin.ensureLoggedIn("/login"), (req, res) => {
   res.render("pages/account");
 });
 app.get("/login", (req, res) => {
@@ -29,7 +58,14 @@ app.get("/register", (req, res) => {
   res.render("pages/register");
 });
 
-app.post("/login", (req, res) => {});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
 app.post("/register", (req, res) => {});
 
